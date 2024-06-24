ea5590952a2254dd7b05ff4c904a891608434bd8
GUTTASIRICHANDANA <148330361+GUTTASIRICHANDANA@users.noreply.github.com>
2024-06-24 10:37:09 +0530

    Add files via upload

---
+++ CS_03 Output.py
@@ -0,0 +1,13 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:/Users/Admin/Desktop/Prodigy/CS-03.py ===============
+Enter a password to check its complexity: SIRI
+Password: SIRI
+Length: 4
+Has Uppercase: True
+Has Lowercase: False
+Has Digit: False
+Has Special Character: False
+Strength: Weak
+Feedback: Your password is weak. Password should be at least 8 characters long. Password should include at least one lowercase letter. Password should include at least one number. Password should include at least one special character (e.g., !, @, #, $, etc.).
